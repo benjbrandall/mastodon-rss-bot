c63ad7c49644276bd3a7ea8e5baec91850a53d38
benjbrandall <ben@process.st>
2018-04-01 17:14:03 +0300

    Update tootbot.rb

--- tootbot.rb
+++ tootbot.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require 'mastodon'
 
 def toot
-  client = Mastodon::REST::Client.new(base_url: "https://botsin.space", bearer_token: "eeaf1a6901476516d21aa9745f10c44cd15ae1b385e661c8a3c403f95a965ab2")
+  client = Mastodon::REST::Client.new(base_url: "https://botsin.space", bearer_token: "BEARER_TOKEN")
   toots = File.open('toots').to_a
   toots.each do |toot|
     client.create_status(toot)
